1308fe98d0a409cc81d4b19d0bfa093c375001af
djalilhebal <abdeldjalil.hebal@gmail.com>
2023-12-12 21:07:18 +0100

    Don't log the currentCount

--- src/app/api/speak/route.ts
+++ src/app/api/speak/route.ts
@@ -2,11 +2,52 @@ import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 
 import NamaeneAzure from '../NamaeneAzure';
-import { getCurrentCount, getNewCountWith } from '../quota';
+import { getNewCountWith } from '../quota';
 
 const HTTP_BAD_REQUEST = 400;
 const HTTP_NOT_MODIFIED = 304;
 const HTTP_SERVICE_UNAVAILABLE = 503;
+
+export async function GET(request: NextRequest) {
+  //const _id = request.headers.get('x-vercel-id');
+
+  const options = {
+    ipa: request.nextUrl.searchParams.get('ipa') || '',
+    voice: request.nextUrl.searchParams.get('voice') || '',
+  };
+
+  // Validate
+  if (!validateOptions(options)) {
+    return new NextResponse(null, { status: HTTP_BAD_REQUEST });
+  }
+
+  // - [ ] Generate ETag
+  // hash(apiVersion + voice + ipa)
+
+  // Check quota
+  // We could do something like "double-checked locking" so we only increment the counter if we aren't above the max.
+  // Pro: The counter stops at the last "exceeding" request. Maybe prevents integer overflows? No.
+  // Con: That means making an additional call to Redis each time.
+  //const usedCharacters = await getCurrentCount();
+  const speakCost = NamaeneAzure.countCharacters(options);
+  const usedCharacters = await getNewCountWith(speakCost);
+  if (usedCharacters > NamaeneAzure.MAX_CHARACTERS_PER_MONTH) {
+    return new NextResponse(null, { status: HTTP_SERVICE_UNAVAILABLE });
+  }
+
+  const data = await NamaeneAzure.speak(options);
+  const contentType = NamaeneAzure.mimeType;
+
+  return new NextResponse(data, {
+    headers: {
+      'Content-Type': contentType,
+      // 1 year
+      'Cache-Control': 'public, max-age=31536000, immutable',
+    },
+    status: 200,
+  });
+
+}
 
 /**
  * @returns is valid?
@@ -25,54 +66,4 @@ function validateOptions(opts: any) {
   }
 
   return true;
-}
-
-export async function GET(request: NextRequest) {
-  const _id = request.headers.get('x-vercel-id');
-
-  const options = {
-    ipa: request.nextUrl.searchParams.get('ipa') || '',
-    voice: request.nextUrl.searchParams.get('voice') || '',
-  };
-
-  // TODO: Remove in production
-  const _currentCount = await getCurrentCount();
-
-  console.log({
-    _id,
-    _currentCount,
-    options,
-  });
-
-  // Validate
-  if (!validateOptions(options)) {
-    return new NextResponse(null, {status: HTTP_BAD_REQUEST});
-  }
-
-  // - [ ] Generate ETag
-  // hash(apiVersion + voice + ipa)
-
-  // Check quota
-  // We could do something like "double-checked locking" so we only increment the counter if we aren't above the max.
-  // Pro: The counter stops at the last "exceeding" request. Maybe prevents integer overflows? No.
-  // Con: That means making an additional call to Redis each time.
-  //const usedCharacters = await getCurrentCount();
-  const speakCost = NamaeneAzure.countCharacters(options);
-  const usedCharacters = await getNewCountWith(speakCost);
-  if (usedCharacters > NamaeneAzure.MAX_CHARACTERS_PER_MONTH) {
-    return new NextResponse(null, {status: HTTP_SERVICE_UNAVAILABLE});
-  }
-  
-  const data = await NamaeneAzure.speak(options);
-  const contentType = NamaeneAzure.mimeType;
-
-  return new NextResponse(data, {
-    headers: {
-      'Content-Type': contentType,
-      // 1 year
-      'Cache-Control': 'public, max-age=31536000, immutable',
-      },
-      status: 200,
-  });
-
 }
